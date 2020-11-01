d8a0ebf4945b6ad40a1ef1be2b5ddc6dbfd7083b
Valmir Memeti <valmirmemeti@gmail.com>
2020-11-01 22:26:34 +0100

    add fs-extra, functionality for deletion of previous m3u files and fix input strings

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const request = require('request');
 const fs = require('fs');
 const { exec, execSync} = require("child_process");
 const readline = require('readline');
+const fsExtra = require('fs-extra')
 
 let configPath = "xtvConfig.json";
-let categoryDict = {};
+let m3uFolderName = "m3u_files";
 
 if (fs.existsSync(configPath)) {
     let configFile = fs.readFileSync(configPath);
@@ -20,22 +21,18 @@ else{
         output: process.stdout
     });
     
-    // ask user for the anme input
-    rl.question("Host (including port)", (host) => {
+    rl.question("Host (including port): ", (host) => {
     
-        // ask for nationality
-        rl.question("Username", (username) => {
+        rl.question("Username: ", (username) => {
 
-            rl.question("Password", (password) => {
-                rl.question("Vlc path", (vlcPath) =>{
+            rl.question("Password: ", (password) => {
+                rl.question("Vlc path (including the executable): ", (vlcPath) =>{
                     rl.close();
                     let configObj = {host: host, username: username, password: password, vlcPath: vlcPath};
                     createConfigJson(JSON.stringify(configObj));
                     initiateXtreamRequests(configObj);
                 });
             });
-                
-            
         });
     
     });
@@ -45,10 +42,15 @@ else{
 //TODO: Refactoring. Some hardcoded strings.
 //TODO: Better function name
 function initiateXtreamRequests(config){
+    let categoryDict = {};
+
     if(config.host.charAt(config.host - 1) != "/") config.host += "/";
     //This is because of Windows' awful handling of spacing in paths...
     if(config.vlcPath.charAt(0) != '"' && config.vlcPath.charAt(config.vlcPath -1) != '"') config.vlcPath = "\"" + config.vlcPath + "\"";
-    
+
+    fsExtra.emptyDirSync(m3uFolderName);
+
+
     let liveCategoriesUrl = config.host + "player_api.php?username=" + config.username + "&password=" + config.password + "&action=get_live_categories";
     request({url: liveCategoriesUrl, json: true}, (error, res, body) => {
         console.log("Fetching live categories...");
@@ -78,7 +80,7 @@ function fetchLiveChannels(config, categoryDict){
         //TODO: Better error-checks
         if (!error && res.statusCode == 200 && Array.isArray(body)) {
             let m3uFileName = createM3u(config, categoryDict, body);
-            execSync(config.vlcPath + " " + m3uFileName);
+            execSync(config.vlcPath + " " + m3uFolderName + "/" + m3uFileName);
         }
 
         else{
@@ -108,7 +110,10 @@ function createM3u(config, categoryDict, liveChannels){
     let completeFileStr = fileHeader + "\n" + fileBody;
     //TODO: Get a better unique filename. This is not unique at all.
     let fileName = Date.now() + ".m3u";
-    fs.appendFileSync(fileName, completeFileStr);
+    if (!fs.existsSync(m3uFolderName)){
+        fs.mkdirSync(m3uFolderName);
+    }
+    fs.appendFileSync(m3uFolderName + "/" + fileName, completeFileStr);
     return fileName;
     
 }
